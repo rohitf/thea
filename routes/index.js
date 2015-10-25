5eed4705fdb2ca248202580be75cf7dabd2a7763
Rohit Falor <rohitfalor@outlook.com>
2015-10-25 16:08:07 -0700

    UI updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile('./public/index.html');
+  res.render('index', {str: 'spanish'})
+});
+
+router.get('/:searchStr', function(req, res, next) {
+  res.render('index', {str: req.params.searchStr})
 });
 
 router.get('/tools', function(req, res, next) {
